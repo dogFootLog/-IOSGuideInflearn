5e672e7ed693001430135e9978f47b91d3c60e83
dogfoot <dogfoot_log@naver.com>
2022-09-22 23:41:43 +0900

    20220922 -  5-6강

--- SwiftGitConnectTest/Lecture_1_Basic.playground/Contents.swift
+++ SwiftGitConnectTest/Lecture_1_Basic.playground/Contents.swift
@@ -69,3 +69,52 @@ httpError.description
 // 5. Optionals_1
 
 // 개념: 값이 있을 수도 있고, 없을 수도 있다.
+// 없는 상태 -> nil
+
+var myAge: Int = 0
+
+/*
+if myAge == nil {
+    // 이건 에러 발생. 타입이 nil도 허용하도록 돼 있지 않기 때문
+    // alert - 나이를 입력해주세요
+}
+*/
+
+var yourAge: Int? = 0
+
+if yourAge == nil {
+    // alert - 나이를 입력해주세요
+}
+
+var myName: String? = ""
+
+if myName == "" {
+    // alert - 이름을 입력해주세요
+}
+
+if myName == nil {
+    // 이름값이 없음
+}
+
+// 옵셔널은 그냥 사용할 수 없는 경우가 많음
+var a: Int? = 10
+var b: Int? = 20
+
+// var c = a + b
+// optional type은 반드시 unwrap 되어야 함
+
+// unwrapped
+// unwrapping
+
+// Int? -> Int
+// String? -> String
+// 벗기지 않으면 더하기도 안되고... 방법
+
+// 방법 1. coalesce
+var c = (a ?? 0) + (b ?? 0)
+
+// force unwrap -> 강제로, 무조건적으로
+// 주의사항: 느낌표에 무조건 값이 있다는 전제 하에 바꾸는 것
+// 값이 만일 없으면 crash남 (크리티컬)
+// 값 있다는 확신 없으면 쓰면 안됨!
+var d = a! + b!
